a26093a7cd0c3e51b69f348c1f3dbfb262b48714
wky95 <wangkayu0512@gmail.com>
2025-07-02 03:23:56 +0800

    add hover different color

--- initial.js
+++ initial.js
@@ -1,34 +1,41 @@
-    const board = document.getElementById("board");
-    const size = 19;
-    let currentPlayer = 'black';
+const board = document.getElementById("board");
+const size = 19;
+let currentPlayer = 'black';
 
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        const cell = document.createElement("div");
-        cell.className = "cell";
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+    const cell = document.createElement("div");
+    cell.className = "cell_black";
+    
+    cell.id = String(i * size + j);
+    cell.dataset.row = i;
+    cell.dataset.col = j;
+    
+    cell.addEventListener("click", function () {
+        if (cell.querySelector(".stone")) return; // 已有棋子不能再放
+
+        const stone = document.createElement("img");
+        stone.className = "stone ";
+        stone.src = "Assets/" + currentPlayer + "stone.png"
+        cell.appendChild(stone);
+
+        // 換邊
+        currentPlayer = (currentPlayer === "black") ? "white" : "black";
         
-        cell.dataset.row = i;
-        cell.dataset.col = j;
-        
-        cell.addEventListener("click", function () {
-          if (cell.querySelector(".stone")) return; // 已有棋子不能再放
+        for (let ii = 0; ii < size * size; ii++) {
+        tmp_cell = document.getElementById(String(ii));
+        tmp_cell.className = "cell_" + currentPlayer;
+        console.log(ii, tmp_cell.dataset.row, tmp_cell.dataset.col)
+        }
+        console.log(cell.className)
+    });
 
-          const stone = document.createElement("img");
-          stone.className = "stone ";
-          stone.src = "Assets/" + currentPlayer + "stone.png"
-          cell.appendChild(stone);
-
-          // 換邊
-          currentPlayer = (currentPlayer === "black") ? "white" : "black";
-          
-        });
-
-        const h_line = document.createElement("div");
-        const v_line = document.createElement("div");
-        h_line.className = "center-line-horizontal"
-        v_line.classList = "center-line-vertical"
-        if (i < size - 1) cell.appendChild(v_line);
-        if (j < size - 1) cell.appendChild(h_line);
-        board.appendChild(cell);
-      }
+    const h_line = document.createElement("div");
+    const v_line = document.createElement("div");
+    h_line.className = "center-line-horizontal"
+    v_line.classList = "center-line-vertical"
+    if (i < size - 1) cell.appendChild(v_line);
+    if (j < size - 1) cell.appendChild(h_line);
+    board.appendChild(cell);
     }
+}
